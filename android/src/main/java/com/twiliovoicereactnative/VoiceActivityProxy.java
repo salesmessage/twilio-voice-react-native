2a400b0ac134bd9fff212bbbc2dcf59520042310
Pavel Zaletski <pzaletski@neklo.com>
2024-03-18 22:05:06 +0300

    SMR-2436 Android: Do Not Request "Devices Nearby" Permission

--- android/src/main/java/com/twiliovoicereactnative/VoiceActivityProxy.java
+++ android/src/main/java/com/twiliovoicereactnative/VoiceActivityProxy.java
@@ -93,11 +93,10 @@ public class VoiceActivityProxy {
     if (Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2) {
       permissionList = new String[] {
         Manifest.permission.RECORD_AUDIO,
-        Manifest.permission.BLUETOOTH_CONNECT,
         Manifest.permission.POST_NOTIFICATIONS };
     } else if (Build.VERSION.SDK_INT > Build.VERSION_CODES.R) {
       permissionList = new String[] {
-        Manifest.permission.RECORD_AUDIO, Manifest.permission.BLUETOOTH_CONNECT };
+        Manifest.permission.RECORD_AUDIO };
     } else {
       permissionList = new String[] { Manifest.permission.RECORD_AUDIO };
     }
