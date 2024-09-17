ae513a47e729c9cfd24ba54a7418800fae2aa68c
Pavel Zaletski <pzaletski@neklo.com>
2024-09-17 12:10:48 +0300

    SMR-2893 Android: Activate sensor to disable screen during the call

--- android/src/main/java/com/twiliovoicereactnative/TwilioVoiceReactNativeModule.java
+++ android/src/main/java/com/twiliovoicereactnative/TwilioVoiceReactNativeModule.java
@@ -60,6 +60,8 @@ public class TwilioVoiceReactNativeModule extends ReactContextBaseJavaModule {
   private static final String SDK_VERSION = "com.twilio.voice.env.sdk.version";
   private final ReactApplicationContext reactContext;
   private final AudioSwitchManager audioSwitchManager;
+  private ProximityManager proximityManager;
+  private EventManager eventManager;
 
   public TwilioVoiceReactNativeModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -82,6 +84,10 @@ public class TwilioVoiceReactNativeModule extends ReactContextBaseJavaModule {
         audioDeviceInfo.putString(VoiceEventType, VoiceEventAudioDevicesUpdated);
         getJSEventEmitter().sendEvent(ScopeVoice, audioDeviceInfo);
       });
+
+    eventManager = new EventManager(reactContext);
+    proximityManager = new ProximityManager(reactContext, eventManager);
+    CallListenerProxy.proximityManager = proximityManager;
   }
 
   /**
@@ -348,6 +354,8 @@ public class TwilioVoiceReactNativeModule extends ReactContextBaseJavaModule {
   @ReactMethod
   public void call_disconnect(String uuid, Promise promise) {
     final CallRecord callRecord = validateCallRecord(reactContext, UUID.fromString(uuid), promise);
+
+    proximityManager.stopProximitySensor();
 
     if (null != callRecord) {
       getVoiceServiceApi().disconnect(callRecord);
